5df2ebeda205f946fa2a643fada6fc43736e4e41
poroh-kun <meta.poroh.kun@gmail.com>
2020-03-24 21:33:15 +0300

    initial commit

---
+++ ScreenSlicer/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ScreenSlicer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKernel container;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ConfigureContainer();
+            ComposeObjects();
+            Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            container = new StandardKernel();
+        }
+
+        private void ComposeObjects()
+        {
+            Current.MainWindow = container.Get<MainWindow>();
+        }
+    }
+}
