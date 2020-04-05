4fa7764758e72a6598caf0838ad3fb650fcf92fa
poroh-kun <meta.poroh.kun@gmail.com>
2020-04-05 17:48:07 +0300

    snap page initial content

---
+++ ScreenSlicer/Converters/ReverseBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ScreenSlicer.Converters
+{
+    public sealed class ReverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return !(bool)value;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return !(bool)value;
+            return true;
+        }
+    }
+}
