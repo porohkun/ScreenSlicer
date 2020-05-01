2ca23e1886f637d267a3859907fec3433a8120e2
poroh-kun <meta.poroh.kun@gmail.com>
2020-05-01 22:09:07 +0300

    run at windows startup feature

---
+++ ScreenSlicer/Updating/RunAtWindowsStartupEvent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using Squirrel;
+using System.IO;
+
+namespace ScreenSlicer.Updating
+{
+    public static class RunAtWindowsStartupEvent
+    {
+        private static RegistryKey OpenRunAtWindowsStartupRegistryKey() =>
+        Registry.CurrentUser.OpenSubKey(
+            "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        public static void CreateRunAtWindowsStartupRegistry(this UpdateManager updateManager)
+        {
+            using (var startupRegistryKey = OpenRunAtWindowsStartupRegistryKey())
+                startupRegistryKey.SetValue(
+                    updateManager.ApplicationName,
+                    Path.Combine(updateManager.RootAppDirectory, $"{updateManager.ApplicationName}.exe"));
+        }
+
+        public static void RemoveRunAtWindowsStartupRegistry(this UpdateManager updateManager)
+        {
+            using (var startupRegistryKey = OpenRunAtWindowsStartupRegistryKey())
+                startupRegistryKey.DeleteValue(updateManager.ApplicationName);
+        }
+    }
+}
