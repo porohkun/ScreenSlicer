1c5ce4e6c3bdda7e6c4c7d85dc61b0d057bf7e7d
poroh-kun <meta.poroh.kun@gmail.com>
2020-07-06 02:44:25 +0300

    fix

--- ScreenSlicer/Settings/Compatibility/Rule.cs
+++ ScreenSlicer/Settings/Compatibility/Rule.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScreenSlicer.Compatibility
 {
