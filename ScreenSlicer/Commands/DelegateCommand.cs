13c2756d58c98cc66845e17092eed295ca53916c
poroh-kun <meta.poroh.kun@gmail.com>
2020-08-04 15:34:35 +0300

    winlistwindow now uses for rules testing

---
+++ ScreenSlicer/Commands/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ScreenSlicer.Commands
+{
+    public class DelegateCommand : InjectableCommand<DelegateCommand>
+    {
+        private Action<object> _executeAction;
+        private Func<object, bool> _canExecuteAction;
+
+        public DelegateCommand(Action<object> executeAction, Func<object, bool> canExecuteAction = null)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        protected override bool CanExecuteInternal(object parameter)
+        {
+            return _canExecuteAction?.Invoke(parameter) ?? true;
+        }
+
+        protected override void ExecuteInternal(object parameter)
+        {
+            _executeAction?.Invoke(parameter);
+        }
+    }
+}
